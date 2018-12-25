b97ea212af381294517b810cdaa8f111fec7693d
dummyforlearning <ghind20@gmail.com>
2018-12-25 12:49:10 +0530

    This is to add more classes

--- src/com/company/TimeTable.java
+++ src/com/company/TimeTable.java
@@ -1,4 +1,17 @@
 package com.company;
 
 public class TimeTable {
+    private ArrayList<Day> dayList;
+
+    public TimeTable(ArrayList<Day> dayList) {
+        this.dayList = dayList;
+    }
+
+    public ArrayList<Day> getDayList() {
+        return dayList;
+    }
+
+    public void setDayList(ArrayList<Day> dayList) {
+        this.dayList = dayList;
+    }
 }
