8cf9fed7701894609f1a44987ba4d704737f5f35
dummyforlearning <ghind20@gmail.com>
2018-12-25 12:45:29 +0530

    New Classes

--- src/com/company/Teacher.java
+++ src/com/company/Teacher.java
@@ -1,4 +1,56 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Teacher {
+    private ArrayList<Subject> listSub;
+    private int teacherId;
+    private String name;
+    private TimeTable timeTable;
+
+    public Teacher(ArrayList<Subject> listSub, int teacherId, String name) {
+        this.listSub = listSub;
+        this.teacherId = teacherId;
+        this.name = name;
+        this.timeTable = null;
+    }
+
+    public Teacher(ArrayList<Subject> listSub, int teacherId, String name, TimeTable timeTable) {
+        this.listSub = listSub;
+        this.teacherId = teacherId;
+        this.name = name;
+        this.timeTable = timeTable;
+    }
+
+    public ArrayList<Subject> getListSub() {
+        return listSub;
+    }
+
+    public void setListSub(ArrayList<Subject> listSub) {
+        this.listSub = listSub;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TimeTable getTimeTable() {
+        return timeTable;
+    }
+
+    public void setTimeTable(TimeTable timeTable) {
+        this.timeTable = timeTable;
+    }
 }
